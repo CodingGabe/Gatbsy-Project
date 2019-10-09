df53e32cc81d9ee49279d8279bb4db978aff1831
Gabe Amaya <gabeamaya2@gmail.com>
2019-10-09 15:38:48 -0500

    setup about, contact and home page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `#38AA9E` }}>
+    <Header headerText="About Membersy" />
+    <Header headerText="Neato" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+        <div style={{ color: '#202020'}}>
+            <Link to="/contact/">Contact</Link>
+            <h1>Hello World!</h1>
+            <p>Welcome to Membersy.</p>
+            <img src="https://source.unsplash.com/random/400x200" alt="rng" />
+        </div>
+    )
