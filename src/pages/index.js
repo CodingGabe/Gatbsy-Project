9cdf588abbe77000af2c941f18a41a67fd5eacb5
Gabe Amaya <gabeamaya2@gmail.com>
2019-10-10 10:36:17 -0500

    add new modules page to display use of css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 export default () => (
         <div>
             <Link to="/contact/">Contact</Link>
+            <Link to="/about-css-modules/">Modules</Link>
             <h1>Hello World!</h1>
             <p>Welcome to Membersy.</p>
             <img src="https://source.unsplash.com/random/400x200" alt="rng" />
